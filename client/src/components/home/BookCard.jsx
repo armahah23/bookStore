72ce10d2c09bdb14ef98b078ffab7a415d3f62d7
armahah23 <armahemp.off@gmail.com>
2024-12-01 23:45:51 +0530

    add BookCard and BookTable components for displaying books with toggle view

---
+++ client/src/components/home/BookCard.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+import PropTypes from "prop-types";
+
+
+function BookCard({ books }) {
+    
+  return (
+    <div className="grid sm:grid-col-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((book)=> (
+        <div key={book._id} className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
+            <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
+                {book.publishYear}
+            </h2>
+            <h4 className="my-2 text-gray-500">{book._id}</h4>
+            <div className="flex items-center justify-start gap-x-4">
+                <PiBookOpenTextLight className="text-2xl text-red-300"/>
+                <h2 className="my-1">{book.title}</h2>
+            </div>
+            <div className="flex items-center justify-start gap-x-4">
+                <BiUserCircle className="text-2xl text-red-300"/>
+                <h2 className="my-1">{book.author}</h2>
+            </div>
+            <div className="flex justify-between items-center gap-x-2 mt-4 p4">
+                <Link to={`book/details/${book._id}`}>
+                    <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
+                </Link>
+                <Link to={`book/edit/${book._id}`}>
+                    <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
+                </Link>
+                <Link to={`book/delete/${book._id}`}>
+                    <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
+                </Link>
+            </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BookCard;
+
+BookCard.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      // Add other book properties here if needed
+    })
+  ).isRequired,
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
+import BookTable from "../components/home/BookTable";
+import BookCard from "../components/home/BookCard";
 
 function Home() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showType, setShowType] = useState("table");
 
   useEffect(() => {
     setLoading(true);
@@ -26,63 +27,34 @@ function Home() {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 :hover-bg-sky-600 px-4 py-1 roundedlg"
+          onClick={() => setShowType("table")}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 :hover-bg-sky-600 px-4 py-1 roundedlg"
+          onClick={() => setShowType("card")}
+        >
+          card
+        </button>
+      </div>
       <div className="flex items-center justify-between border-b border-neutral-300 px-2 mb-2 rounded-md">
-        <h1 className="text-5xl my-6 text-sky-600 font-extrabold tracking-wide">Books List</h1>
+        <h1 className="text-5xl my-6 text-sky-600 font-extrabold tracking-wide">
+          Books List
+        </h1>
         <Link to="/book/create">
           <MdOutlineAddBox className="text-4xl text-sky-800" />
         </Link>
       </div>
       {loading ? (
         <Spinner />
+      ) : showType === "table" ? (
+        <BookTable books={books} />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr className="text-white bg-gradient-to-r from-sky-500 to-sky-800 rounded-xl">
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Author
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Publish Year
-              </th>
-              <th className="border border-slate-600 rounded-md">Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book, index) => (
-              <>
-                <tr key={book._id} className="h-8">
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {index + 1}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {book.title}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.author}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.publishYear}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2x1 text-green-800" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2x1 text-yellow-600" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2x1 text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </table>
+        <BookCard books={books} />
       )}
     </div>
   );
